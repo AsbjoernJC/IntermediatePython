30a193a4056820abc842bf0af49f8a0cc8ae3766
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2020-09-07 20:45:05 +0200

    added folders to git ignore

--- Data structures/Big O notation space growth.py
+++ Data structures/Big O notation space growth.py
@@ -1,2 +1,2 @@
 # The point at which i stopped:
-# https://www.youtube.com/watch?v=IR_S8BC8KI0&list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&index=2
+# https://youtu.be/IR_S8BC8KI0?list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&t=446
